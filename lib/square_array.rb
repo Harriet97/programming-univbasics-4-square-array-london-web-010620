625a5362c4b5557725db1f47571edc21d541cff3
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-06 11:41:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,19 @@ def square_array(numbers)
   new_numbers
 end
 
+def square_array(numbers)
+  counter = 0 
+  while numbers[counter] do
+    new_numbers = numbers[counter] ** 2
+    counter += 1
+  end
+  new_numbers
+end
+
+def square_array(numbers)
+  count = 0
+  while count < numbers.length do
+    numbers.index ** 2 
+  end
+  square_array(numbers)
+end 
