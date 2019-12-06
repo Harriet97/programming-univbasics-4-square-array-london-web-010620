c0ef2ef83a96da02fe71e6830ce15c7bb1c2cdf2
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-06 11:44:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(numbers)
   i = 0 
   while i < numbers.length do
-    new_numbers = numbers[i]**2
+    new_numbers[i] = numbers[i]**2
     i += 1
   end
-  new_numbers
+  new_numbers[i]
 end
 
 
